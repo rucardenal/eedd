5e27dbfe653ed601f225e72c32f3dd3f324cf817
unknown <r.cardenal.2025@dcloud.local>
2026-02-16 18:59:07 +0100

    16 FEB 2026 6:59 PM

--- Ejercicios/20260211-T2/Practica 1 Complejos/circuito.c
+++ Ejercicios/20260211-T2/Practica 1 Complejos/circuito.c
@@ -3,3 +3,33 @@
 //
 
 #include "circuito.h"
+
+#define PI 3.14159265
+
+void fImpedanciaL (float r1, float r2, tComplejo *pResultado) {
+    fCrearComplejo(0, 1*r1*r2, pResultado);
+}
+
+void fImpedanciaR (float r, tComplejo *pResultado) {
+    fCrearComplejo(r, 0, pResultado);
+}
+
+void fImpedanciaC (float r1, float r2, tComplejo *pResultado) {
+    fCrearComplejo(0, (1/1*r1*r2), pResultado);
+}
+
+void fImpedanciaTotal (tComplejo c1, tComplejo c2, tComplejo c3, tComplejo *pResultado) {
+
+}
+
+void fFrecuenciaAngular (float r, float *pResultado) {
+    fCrearComplejo(2*PI*r, 0, pResultado);
+}
+
+void fIntensidad (tComplejo c1, tComplejo c2, tComplejo *pResultado) {
+
+}
+
+void fCaidaPotencial (tComplejo c1, tComplejo c2, tComplejo *pResultado) {
+   
+}
